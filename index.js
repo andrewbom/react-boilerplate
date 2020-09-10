dc1f4c428495e358d652f0511468531ad2983e5f
andrewbom <59633536+andrewbom@users.noreply.github.com>
2020-09-10 15:33:44 +1000

    move the private information into dev.js

--- index.js
+++ index.js
@@ -1,11 +1,27 @@
 const express = require("express");
 const app = express();
 const mongoose = require("mongoose");
+const bodyParser = require("body-parser");
+var cookieParser = require("cookie-parser");
+const config = require("./config/key");
+
+const { User } = require("./models/user");
+
+// --------------------------------- middlewares setup ---------------------------------
+
+//to not get any deprecation warning or error
+//support parsing of application/x-www-form-urlencoded post data
+app.use(bodyParser.urlencoded({ extended: true }));
+//to get json data
+// support parsing of application/json type post data
+app.use(bodyParser.json());
+app.use(cookieParser());
+
+// --------------------------------- END middlewares setup ---------------------------------
 
 // --------------------------------- mongoDB setup ---------------------------------
-const mongoUri =
-  "mongodb+srv://admin:passwordpassword@cluster0.4z08s.mongodb.net/boilerplate?retryWrites=true&w=majority";
-mongoose.connect(mongoUri, {
+
+mongoose.connect(config.mongoURI, {
   useNewUrlParser: true,
   useCreateIndex: true,
   useUnifiedTopology: true,
@@ -23,6 +39,16 @@ mongoose.connection.on("error", (err) => {
 
 app.get("/", (req, res) => {
   res.send("hello world");
+});
+
+app.post("/api/users/register", (req, res) => {
+  const user = new User(req.body);
+  user.save((err, userData) => {
+    if (err) return res.json({ success: false, err });
+    return res.status(200).json({
+      success: true,
+    });
+  });
 });
 
 // --------------------------------- END Route setup ---------------------------------
