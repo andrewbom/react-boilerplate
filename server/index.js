7491c96d419bc56a59db039ab1cb87dadec48d37
andrewbom <59633536+andrewbom@users.noreply.github.com>
2020-09-10 19:51:30 +1000

    initialize deployment

--- server/index.js
+++ server/index.js
@@ -4,8 +4,8 @@ const mongoose = require("mongoose");
 const bodyParser = require("body-parser");
 var cookieParser = require("cookie-parser");
 const config = require("./config/key");
-
 const { User } = require("./models/user");
+const { auth } = require("./middleware/auth");
 
 // --------------------------------- middlewares setup ---------------------------------
 
@@ -41,11 +41,58 @@ app.get("/", (req, res) => {
   res.send("hello world");
 });
 
-app.post("/api/users/register", (req, res) => {
+app.get("/api/user/auth", auth, (req, res) => {
+  res.status(200).json({
+    _id: req.user._id,
+    // isAdmin: req.user.role === 0 ? false : true,
+    isAuth: true,
+    email: req.user.email,
+    name: req.user.name,
+    lastname: req.user.lastname,
+    role: req.user.role,
+    // image: req.user.image,
+  });
+});
+
+app.post("/api/user/register", (req, res) => {
   const user = new User(req.body);
   user.save((err, userData) => {
     if (err) return res.json({ success: false, err });
     return res.status(200).json({
+      success: true,
+      userData: userData,
+    });
+  });
+});
+
+app.post("/api/user/login", (req, res) => {
+  User.findOne({ email: req.body.email }, (err, user) => {
+    if (!user)
+      return res.json({
+        loginSuccess: false,
+        message: "Auth failed, email not found",
+        err,
+      });
+
+    user.comparePassword(req.body.password, (err, isMatch) => {
+      if (!isMatch) return res.json({ loginSuccess: false, message: "Wrong password", err });
+
+      user.generateToken((err, user) => {
+        if (err) return res.status(400).send(err);
+        res.cookie("w_authExp", user.tokenExp);
+        res.cookie("w_auth", user.token).status(200).json({
+          loginSuccess: true,
+          userId: user._id,
+        });
+      });
+    });
+  });
+});
+
+app.get("/api/user/logout", auth, (req, res) => {
+  User.findOneAndUpdate({ _id: req.user._id }, { token: "", tokenExp: "" }, (err, doc) => {
+    if (err) return res.json({ success: false, err });
+    return res.status(200).send({
       success: true,
     });
   });
@@ -53,4 +100,8 @@ app.post("/api/users/register", (req, res) => {
 
 // --------------------------------- END Route setup ---------------------------------
 
-app.listen(5000);
+const port = process.env.PORT || 5000;
+
+app.listen(port, () => {
+  console.log(`Server Listening on ${port}`);
+});
---
+++ server/middleware/auth.js
@@ -0,0 +1,21 @@
+const mongoose = require("mongoose");
+const User = mongoose.model("User");
+
+let auth = (req, res, next) => {
+  let token = req.cookies.w_auth;
+
+  User.findByToken(token, (err, user) => {
+    if (err) throw err;
+    if (!user)
+      return res.json({
+        isAuth: false,
+        error: true,
+      });
+
+    req.token = token;
+    req.user = user;
+    next();
+  });
+};
+
+module.exports = { auth };
